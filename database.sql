9a3efaff06692899dac3eddc2ee4cd800e95b6eb
Mandy27 <giaman27@gmail.com>
2016-03-02 02:18:38 -0800

    Finished setting up database

---
+++ database.sql
@@ -0,0 +1,12 @@
+﻿/*CREATE TABLE AdjusterReport(
+	campaign_startdate DATE NOT NULL,
+	campaign_cpm TEXT,
+	campaign_id INTEGER NOT NULL,
+	campaign_name TEXT NOT NULL,
+	total_clicks INTEGER,
+	total_views INTEGER,
+	PRIMARY KEY (campaign_id)
+)*/
+--DROP TABLE AdjusterReport;
+
+SELECT * FROM AdjusterReport;
--- src/client/HttpClient.java
+++ src/client/HttpClient.java
@@ -3,6 +3,10 @@ package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -13,15 +17,39 @@ import org.apache.http.impl.client.HttpClients;
 import com.google.gson.Gson;
 
 public class HttpClient {
-	static Campaign[] campaignList;
-	static Creative[] creativeList;
+	private static final Campaign[] NO_CAMPAIGN = {};
+	private static final Creative[] NO_CREATIVE = {};
 	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
 		
+		/************ DATABASE CONNECTION **************/
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		Connection connection = null;
+        try {
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/AdjusterHomework", "Mandy",
+					"");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed!");
+			e.printStackTrace();
+			return;
+		}
+        if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Connection failed!");
+		}
+//        Campaign[] campaignList = getCampaigns();
+//		Creative[] creativeList = getCreatives();
 	}
 
-	public static void getCampaigns() throws IOException{
+	public static Campaign[] getCampaigns() throws IOException{
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet("http://54.172.67.92:8080/api/campaign");
 		CloseableHttpResponse response = httpclient.execute(httpget);
@@ -40,7 +68,7 @@ public class HttpClient {
 			}
 			// Convert String to JsonArray
 			Gson gson = new Gson();
-			campaignList = gson.fromJson(campaignStringList.toString(), Campaign[].class);
+			return gson.fromJson(campaignStringList.toString(), Campaign[].class);
 
 		}catch (ClientProtocolException e) {
 			e.printStackTrace();	 
@@ -49,9 +77,10 @@ public class HttpClient {
 		}finally {
 			response.close();
 		}
+		return NO_CAMPAIGN;
 	}
 	
-	public static void getCreatives() throws IOException{
+	public static Creative[] getCreatives() throws IOException{
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet("http://54.172.67.92:8080/api/creative ");
 		CloseableHttpResponse response = httpclient.execute(httpget);
@@ -70,7 +99,7 @@ public class HttpClient {
 			}
 			// Convert String to JsonArray
 			Gson gson = new Gson();
-			creativeList = gson.fromJson(creativeStringList.toString(), Creative[].class);
+			return gson.fromJson(creativeStringList.toString(), Creative[].class);
 
 		}catch (ClientProtocolException e) {
 			e.printStackTrace();	 
@@ -79,6 +108,7 @@ public class HttpClient {
 		}finally {
 			response.close();
 		}
+		return NO_CREATIVE;
 	}
 
 }
